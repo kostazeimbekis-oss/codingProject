5f4b919c8e04254128f36bba3559e9ef31c171e7
kostazeimbekis-oss <kostazeimbekis@gmail.com>
2025-11-02 13:32:43 -0500

    numner 56

--- main.go
+++ main.go
@@ -5,5 +5,28 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(countkmers("ACGAGGTACGA", 3))
 }
+
+
+
+
+type Kmers struct{
+	sequence string
+	count int
+}
+
+
+
+
+
+func countkmers(sequence string, k int) map[string]int {
+	var kmers = make(map[string]int)
+
+	for i := 0; i < len(sequence) - k + 1; i++ {
+			// accumulate k-mers
+	}
+
+	return kmers
+}
+
